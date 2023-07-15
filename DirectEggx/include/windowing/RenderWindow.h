be699993436408ae91c6db63d0fae5b39b915e3e
Gary Tang <46045360+Lelloq@users.noreply.github.com>
2023-07-15 15:45:02 +0100

    comments

--- DirectEggx/include/windowing/RenderWindow.h
+++ DirectEggx/include/windowing/RenderWindow.h
@@ -4,11 +4,39 @@
 class RenderWindow
 {
 public:
+	/**
+	 * \fn bool Init(HINSTANCE hInstance, std::string title, std::string wClass, int width, int height).
+	 * \brief Initialises the window using win32.
+	 * 
+	 * \param hInstance Window instance
+	 * \param title Window title
+	 * \param wClass Window class
+	 * \param width Width
+	 * \param height Height
+	 * \return 
+	 */
 	bool Init(HINSTANCE hInstance, std::string title, std::string wClass, int width, int height);
+
+	/**
+	 * \fn bool ProcessMessages().
+	 * \brief Processess input
+	 * 
+	 * \return 
+	 */
 	bool ProcessMessages();
+
+	/**
+	 * \brief Window destructor.
+	 * 
+	 */
 	~RenderWindow();
 private:
+	/**
+	 * \fn void RegisterWindowClass().
+	 * \brief Creates a window class for the window instance.
+	 */
 	void RegisterWindowClass();
+
 	HWND _handle = NULL;
 	HINSTANCE _hInstance = NULL;
 	std::string _window_title = "";
